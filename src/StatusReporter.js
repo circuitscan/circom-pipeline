36a8d70c9b5d9ee17dfcce265d45380bfc653bbb
numtel <ben@latenightsketches.com>
2024-09-10 15:25:09 -0400

    Throttle on status logging to s3

--- src/StatusReporter.js
+++ src/StatusReporter.js
@@ -1,4 +1,4 @@
-import {uploadJsonToS3, getDiskUsage} from './utils.js';
+import { uploadJsonToS3, getDiskUsage } from './utils.js';
 
 // For small logs
 export class StatusReporter {
@@ -7,9 +7,11 @@ export class StatusReporter {
     this.key = key;
     this.logs = [];
     this.memoryInterval = null;
+    this.lastUploadTime = 0; // Tracks last time uploadJsonToS3 was called
   }
+
   startMemoryLogs(timeout) {
-    if(this.memoryInterval) throw new Error('ALREADY_LOGGING_MEMORY');
+    if (this.memoryInterval) throw new Error('ALREADY_LOGGING_MEMORY');
     this.memoryInterval = setInterval(async () => {
       this.log('Memory Usage Update', {
         memory: process.memoryUsage(),
@@ -17,18 +19,29 @@ export class StatusReporter {
       });
     }, timeout);
   }
+
   stopMemoryLogs() {
-    if(this.memoryInterval) {
+    if (this.memoryInterval) {
       clearInterval(this.memoryInterval);
       this.memoryInterval = null;
     }
   }
+
   async log(msg, data) {
+    const currentTime = Date.now();
+
+    // Add log entry
     this.logs.push({
       msg,
       data,
       time: process.uptime(),
     });
-    await uploadJsonToS3(this.bucket, this.key, this.logs);
+
+    // Check if 5 seconds have passed since last upload
+    if (currentTime - this.lastUploadTime >= 5000) {
+      await uploadJsonToS3(this.bucket, this.key, this.logs);
+      this.lastUploadTime = currentTime; // Update the last upload time
+    }
   }
 }
+
